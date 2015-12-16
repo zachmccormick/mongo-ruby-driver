1084deda39eb4c80cdbdb7b14f4cc68f9a3c0c29
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2015-12-16 15:22:07 -0500

    For writes, retry a NoServerAvailable one time because we may have hit a condition of socket hanging on cluster.scan! and we want to be able to recover from that.

--- lib/mongo/retryable.rb
+++ lib/mongo/retryable.rb
@@ -109,26 +109,30 @@ module Mongo
     def write_with_retry_helper(attempt, &block)
       begin
         block.call
-      rescue Error::SocketError, Error::SocketTimeoutError, Error::OperationFailure => e
+      rescue Error::SocketError, Error::SocketTimeoutError, Error::OperationFailure, Error::NoServerAvailable => e
         connection_error = e.kind_of?(Error::SocketError) || e.kind_of?(Error::SocketTimeoutError)
         operation_failure = e.kind_of?(Error::OperationFailure)
+        no_server_available = e.kind_of?(Error::NoServerAvailable)
         runner_dead = e.message.include?(RUNNER_DEAD)
         not_master = e.message.include?(NOT_MASTER) || e.message.include?(NOT_CONTACT_PRIMARY)
         batch_write = e.message.include?('no progress was made executing batch write op'.freeze)
-        if connection_error || not_master || batch_write
+        if connection_error || not_master || batch_write || no_server_available
           if connection_error
             Mongo::Logger.logger.warn("[jontest] got connection error in write on #{cluster.servers.inspect}, attempt #{attempt}")
           elsif not_master
             Mongo::Logger.logger.warn("[jontest] got not master in write on #{cluster.servers.inspect}, attempt #{attempt}")
           elsif batch_write
             Mongo::Logger.logger.warn("[jontest] got batch write failure in write on #{cluster.servers.inspect}, attempt #{attempt}")
+          elsif no_server_available
+            Mongo::Logger.logger.warn("[jontest] got no server available in write on #{cluster.servers.inspect}, will retry one more time")
+            attempt = cluster.max_read_retries - 1
           end
           rescan!
         end
         if runner_dead
           Mongo::Logger.logger.info("[jontest] got RUNNER_DEAD in write on #{cluster.servers.inspect}, attempt #{attempt}")
         end
-        if connection_error || (operation_failure && (e.retryable? || e.unauthorized?)) || runner_dead
+        if connection_error || (operation_failure && (e.retryable? || e.unauthorized?)) || runner_dead || no_server_available
           # We're using max_read_retries here but if we got one of the errors that is causing us to be here, we should be retrying
           # often anyway
           if attempt < cluster.max_read_retries
